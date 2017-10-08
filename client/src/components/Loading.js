969e8c3c377ccb455537363f1a415e3fe48b946c
Andrew Godwin <andrew.godwin86@gmail.com>
2017-10-08 20:01:13 +1100

    Centered loading component

--- client/src/components/Loading.js
+++ client/src/components/Loading.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Loading = () => (
-  <div className="Loading">
+  <div className="loading">
     <svg width="44" height="44" viewBox="0 0 44 44" xmlns="http://www.w3.org/2000/svg" stroke="#4285F4">
       <g fill="none" fillRule="evenodd" strokeWidth="2">
         <circle cx="22" cy="22" r="1">
