170b67af308fb781ea85e07cb895815381451dcc
Andrew Godwin <andrew.godwin86@gmail.com>
2017-10-08 14:10:21 +1100

    Put nav bar into separate Navigation component

---
+++ client/src/components/Navigation.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+
+export default class Navigation extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="dark" dark expand="md">
+          <NavbarBrand href="/">Nexus Graphql</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="mr-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">Entities</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://developer.knowledge-architecture.com">Nexus REST API</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
