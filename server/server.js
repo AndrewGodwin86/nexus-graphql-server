8df1dec4323352752559f83bf36fc80d59ee84e3
Andrew Godwin <andrew.godwin86@gmail.com>
2017-10-08 13:20:30 +1100

    Include auth headers properly in server

--- server/server.js
+++ server/server.js
@@ -4,15 +4,21 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 
 import { schema } from './src/schema';
+import { context } from './src/resolvers';
 
 const PORT = 4000;
 const server = express();
 
+if (typeof process.env.AuthHeader === 'undefined') {
+  console.warn('WARNING: process.env.AuthHeader is not defined. Check README.md for more information');
+}
+
 server.use('*', cors({ origin: 'http://localhost:3000' }));
 
-server.use('/graphql', bodyParser.json(), graphqlExpress({
-  schema
-}));
+server.use('/graphql', bodyParser.json(), graphqlExpress(request => ({
+  schema,
+  context: context(request.headers, process.env)
+})));
 
 server.use('/graphiql', graphiqlExpress({
   endpointURL: '/graphql'
