a70f3e0150a868f2a507f01c1e20e1520dea726a
Andrew Godwin <andrew.godwin86@gmail.com>
2017-10-08 12:59:59 +1100

    Allow CORS use for connection to client

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
 import express from 'express';
-import {
-  graphqlExpress,
-  graphiqlExpress,
-} from 'graphql-server-express';
+import { graphqlExpress, graphiqlExpress } from 'graphql-server-express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 import { schema } from './src/schema';
 
 const PORT = 4000;
 const server = express();
+
+server.use('*', cors({ origin: 'http://localhost:3000' }));
 
 server.use('/graphql', bodyParser.json(), graphqlExpress({
   schema
